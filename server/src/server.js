f1ad24b39c3ea282cb72768f5ab147009f96a72a
Nithin-Kumar127 <nithinkumarj.mail@gmail.com>
2025-10-20 12:21:33 +0530

    chore: initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 
 import { ENV } from "./config/env.js";
+import { connectDB as connect } from "./config/db.js";
 
 const app = express();
-
-app.listen(ENV.PORT, () => {
-  console.log("Server is running on port", ENV.PORT);
-});
 
 app.get("/", (req, res) => {
   res.send("Hello World!1234");
 });
 
 console.log("mongo uri:", ENV.MONGO_URI); 
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is running on port", ENV.PORT);
+  connect(); 
+});
