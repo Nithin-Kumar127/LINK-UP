2d8fdbd31b0b197311ac1511b3498a6ddf601fca
Nithin-Kumar127 <nithinkumarj.mail@gmail.com>
2025-10-20 17:45:17 +0530

    server.js 2nd change

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,8 @@
 import express from "express";
-import {clerkMiddleware} from "@clerk/express";
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
 import { connectDB as connect } from "./config/db.js";
 import { inngest } from "./config/inngest.js";
-
 
 const app = express();
 
@@ -11,30 +10,29 @@ app.get("/", (req, res) => {
   res.send("Hello World!1234");
 });
 
-app.use(clerKMiddleware());
+app.use(clerkMiddleware());
 
 app.use(express.json());
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-console.log("mongo uri:", ENV.MONGO_URI); 
-
+console.log("mongo uri:", ENV.MONGO_URI);
 
 const startServer = async () => {
   try {
     await connect();
     if (ENV.NODE_ENV != "production") {
       app.listen(ENV.PORT, () => {
-        console.log(`Server running in ${ENV.NODE_ENV} mode on port ${ENV.PORT}`);
+        console.log(
+          `Server running in ${ENV.NODE_ENV} mode on port ${ENV.PORT}`
+        );
       });
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.error("Error starting server:", error);
     process.exit(1);
   }
-}; 
-
+};
 
 startServer();
 
